efc4ffc86d156c0004bcafb0788195fbf031a427
LUCAS VERISSIMO FERIAN DA SILVA <24.85868-0@maua.br>
2024-03-14 22:22:54 -0300

    apostila02(git-js): funcao dividir implementada

--- calc.js
+++ calc.js
@@ -1,17 +1,12 @@
-function soma(x,y){
-    return x + y
+module.exports = {
+    soma: (x,y) => x+y,
+    subtracao: function(x,y) {
+        return x-y
+    },
+    multiplicacao: (x,y) => {
+        return x*y
+    },
+    divisao: function (x,y) {
+        return x/y
+    }
 }
-
-const subtracao = (x,y) => x - y
-
-const multiplicacao = function (x,y){
-    return x * y
-}
-
-function executar(operacao, x,y){
-    return operacao(x,y)
-}
-
-console.log(soma(2,3))
-console.log(subtracao(2,3))
-console.log(multiplicacao(2,3))
